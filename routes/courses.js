da6d9ce89c85fa8f2e6c050843d593ba13ef4c59
David Adams <onethinline@gmail.com>
2020-09-08 15:38:37 -0700

    Add authentication and access control

--- routes/courses.js
+++ routes/courses.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+
 const router = express.Router();
 const Course = require('../models').Course;
 const User = require('../models').User;
@@ -13,6 +16,43 @@ function asyncHandler(cb){
     }
   }
 }
+
+// Authentication code adapted from Treehouse REST API Auth with Express workshop
+const authenticateUser = async (req, res, next) => {
+  let message = null;
+
+  // Parse the user's credentials from the Authorization header.
+  const credentials = auth(req);
+
+  if (credentials) {
+    const user = await User.findOne({
+      where: {
+        emailAddress: credentials.name
+      }
+    });
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.emailAddress}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+
+  // If user authentication failed...
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
 
 // GET /api/courses 200 - Returns a list of courses (including the user that owns each course)
 router.get('/', asyncHandler(async (req, res) => {
@@ -56,7 +96,7 @@ router.get('/:id', asyncHandler(async (req, res, next) => {
 }));
 
 // POST /api/courses 201 - Creates a course, sets the Location header to the URI for the course, and returns no content
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', authenticateUser, asyncHandler(async (req, res, next) => {
   try {
     const course = await Course.create(req.body);
     res.status(201).location('/api/courses/' + course.id).end();
@@ -67,11 +107,18 @@ router.post('/', asyncHandler(async (req, res, next) => {
 }));
 
 // PUT /api/courses/:id 204 - Updates a course and returns no content
-router.put('/:id', asyncHandler(async (req, res, next) => {
+router.put('/:id', authenticateUser, asyncHandler(async (req, res, next) => {
   let course;
   try {
     course = await Course.findByPk(req.params.id);
     if (course) {
+      // Ensure the authenticated user is the matched course's owner
+      if (course.userId != req.currentUser.id) {
+        const error = new Error("User not authorized to modify this record");
+        error.status = 403;
+        next(error);
+      }
+
       await course.update(req.body);
       res.status(204).end();
     } else {
@@ -85,9 +132,16 @@ router.put('/:id', asyncHandler(async (req, res, next) => {
 }));
 
 // DELETE /api/courses/:id 204 - Deletes a course and returns no content
-router.delete('/:id', asyncHandler(async (req, res, next) => {
+router.delete('/:id', authenticateUser, asyncHandler(async (req, res, next) => {
   const course = await Course.findByPk(req.params.id);
   if (course) {
+    // Ensure the authenticated user is the matched course's owner
+    if (course.userId != req.currentUser.id) {
+      const error = new Error("User not authorized to modify this record");
+      error.status = 403;
+      next(error);
+    }
+    
     await course.destroy();
     res.status(204).end();
   } else {
--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+
 const router = express.Router();
 const User = require('../models').User;
 
@@ -13,9 +16,66 @@ function asyncHandler(cb){
   }
 }
 
-// GET /api/users 200 - Returns the currently authenticated user
+// Authentication code adapted from Treehouse REST API Auth with Express workshop
+const authenticateUser = async (req, res, next) => {
+  let message = null;
 
+  // Parse the user's credentials from the Authorization header.
+  const credentials = auth(req);
+
+  if (credentials) {
+    const user = await User.findOne({
+      where: {
+        emailAddress: credentials.name
+      }
+    });
+    if (user) {
+      const authenticated = bcryptjs
+        .compareSync(credentials.pass, user.password);
+      if (authenticated) {
+        req.currentUser = user;
+      } else {
+        message = `Authentication failure for username: ${user.emailAddress}`;
+      }
+    } else {
+      message = `User not found for username: ${credentials.name}`;
+    }
+  } else {
+    message = 'Auth header not found';
+  }
+
+  // If user authentication failed...
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+};
+
+// GET /api/users 200 - Returns the currently authenticated user
+router.get('/', authenticateUser, asyncHandler(async (req, res) => {
+  const user = req.currentUser;
+  res.json({ 
+    firstName: user.firstName,
+    lastName: user.lastName,
+    emailAddress: user.emailAddress
+  });
+}));
 
 // POST /api/users 201 - Creates a user, sets the Location header to "/", and returns no content
+router.post('/', asyncHandler(async (req, res, next) => {
+  try {
+    const user = req.body;
+
+    // Hash the user's password
+    user.password = bcryptjs.hashSync(user.password);
+
+    await User.create(user);
+    res.status(201).location('/').end();
+  } catch (error) {
+    // Handle errors
+  }
+}));
 
 module.exports = router;
